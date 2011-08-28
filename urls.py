60bb735b06455b3ff477c8efff5b33e3e32fb03a
Wesley Zhao <wesley.zhao@gmail.com>
2011-08-27 21:59:12 -0700

    changes

--- urls.py
+++ urls.py
@@ -1,4 +1,7 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.contrib import admin
+admin.autodiscover()
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -10,8 +13,11 @@ urlpatterns = patterns('',
     # url(r'^amazon_charity/', include('amazon_charity.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
+                       url(r'[(index)]*$','engine.views.index'),
 )
+
+urlpatterns += staticfiles_urlpatterns()
