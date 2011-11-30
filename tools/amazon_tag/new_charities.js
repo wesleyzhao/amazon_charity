9440198413ac2e78abcca2ba9765384b8f975f28
Wesley Zhao <wesley.zhao@gmail.com>
2011-11-30 06:37:23 -0800

    added cool json loader feature for public list of charities

---
+++ tools/amazon_tag/new_charities.js
@@ -0,0 +1,34 @@
+var charitiesLink = "http://dl.dropbox.com/u/19699329/charities.json";
+
+function getJSONByURL(url){
+
+    var charitiesArr = {};
+    charitiesArr = $.ajax({
+	url: url,
+	dataType: 'json',
+	async: false
+    });
+    return JSON.parse(charitiesArr.responseText);
+
+}
+
+function placeCharitiesByDiv(divID){
+    
+    var el = document.getElementById(divID);
+    var charr = getJSONByURL(charitiesLink);
+
+    for (charity in charr){
+	var filler = document.createElement('p');
+	var new_charity = document.createElement('div');
+	new_charity.setAttribute('class', 'new-charity');
+	new_charity.innerHTML = "<span class='charity-name'>" + charity + "</span>: <span class='charity-tag'><a href='#' onclick='addCharityTagToInput(\"" + charr[charity] + "\", \"tag-input\")'>" + charr[charity] + "</a></span>";
+	filler.appendChild(new_charity); // put the new charity in the <p> tags
+	el.appendChild(filler); // put the new charity and <p> in the element
+    }
+
+}
+
+function addCharityTagToInput(tag, inputId){
+    var inputEl = document.getElementById(inputId);
+    inputEl.value = tag;
+}
--- tools/amazon_tag/popup.js
+++ tools/amazon_tag/popup.js
@@ -2,3 +2,11 @@ function openNewPageWithURL(url){
     chrome.tabs.create({'url' : url}, function(tab){
     });
 }
+
+function populateCharitiesList(){
+
+    placeCharitiesByDiv('charity-list'); // method inherited from new_charities.js
+    charity_el = document.getElementById('charity-loader-link');
+    charity_el.innerHTML = charity_el.innerText;
+
+}
